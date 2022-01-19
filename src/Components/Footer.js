3f9a45c0833f171c16ac0418909ce4580a1331d9
EraOfCoding <63988030+EraOfCoding@users.noreply.github.com>
2022-01-19 14:47:20 +0600

    logos

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import '../Style/Footer.css';
 
+import nis_logo from '../Images/NIS_logo.png'
+import mootcourt_logo from '../Images/mootcourt_logo.jpeg'
+import girlupnis_logo from '../Images/girlupnis_logo.jpeg'
+
 function Footer() {
     return (
-        <div className="footer">
-            <h6>Moot Court Kazakhstan for High School</h6>
-            <div className='footer-contacts'>
-                <p>Эл. адрес: mootcourt.kz@gmail.com</p>
-                <p>Тел.: +7-775-985-26-27</p>
-                <p>Instagram: girl.up.nis</p>
+        <div className="footer-container">
+            <div className="footer">
+                <h6>Moot Court Kazakhstan for High School</h6>
+                <div className='footer-contacts'>
+                    <p>Эл. адрес: mootcourt.kz@gmail.com</p>
+                    <p>Тел.: +7-775-985-26-27</p>
+                    <p>Instagram: girl.up.nis</p>
+                </div>
             </div>
-        </div>
+            <div className='footer-sponsors'>
+                <img className='org-logo' src={nis_logo} />
+                <img className='org-logo' src={mootcourt_logo} />
+                <img className='org-logo' src={girlupnis_logo} />
+            </div>
+        </div >
     );
 }
 
