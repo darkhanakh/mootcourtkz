82e86363b76ba2ceba5ab9e70a10023a19be8cd2
Zhanibek <zhanibek-2005@bk.ru>
2022-07-06 05:56:11 +0600

    Added i18next

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import '../Style/Landing.css'
+import React from "react";
+import "../Style/Landing.css";
+import { useTranslation } from "react-i18next";
 
 function Landing(props) {
-    return (
-        <div className="landing-container">
-            <div className='landing-body'>
-                <div className='landing-body-text'>
-                    <h2>Moot Court Kazakhstan for High School</h2>
-                    <h6>-игровой судебный процесс-</h6>
-                </div>
-                <button onClick={props.scroll}>Подробнее</button>
-            </div>
+  const { t } = useTranslation();
+  return (
+    <div className="landing-container">
+      <div className="landing-body">
+        <div className="landing-body-text">
+          <h2>Moot Court Kazakhstan for High School</h2>
+          <h6>-игровой судебный процесс-</h6>
         </div>
-    )
+        <button onClick={props.scroll}>Подробнее</button>
+      </div>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import '../Style/Navbar.css';
-import { Link } from 'react-router-dom';
-
+import React, { useState, useTransition } from "react";
+import "../Style/Navbar.css";
+import { Link } from "react-router-dom";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+import I18NextHttpBackend from "i18next-http-backend";
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
@@ -17,52 +20,95 @@ class Navbar extends React.Component {
       this.setState({ minimenu: false });
     }
   };
-
   render() {
     return (
       <div className="navbar">
         <Link id="navbar-home-btn" to="/">
           Moot Court Kazakhstan for High School
         </Link>
-        <div className={`minimenu ${this.state._color}`} onClick={this.handleExpand}>
+        <div
+          className={`minimenu ${this.state._color}`}
+          onClick={this.handleExpand}
+        >
           <div></div>
           <div></div>
           <div></div>
         </div>
-        <div className={`links ${this.state.minimenu ? `active` : ''}`}>
+        <div className={`links ${this.state.minimenu ? `active` : ""}`}>
+          <Link
+            to="#"
+            className="navbar-link"
+            style={{ textDecoration: "none" }}
+          >
+            <Link
+              to="#"
+              className="navbar-link"
+              style={{ textDecoration: "none", padding: "2px" }}
+              onClick={() => {
+                i18n.changeLanguage("ru");
+              }}
+            >
+              рус |
+            </Link>
+            <Link
+              to="#"
+              className="navbar-link"
+              style={{ textDecoration: "none", padding: "2px" }}
+              onClick={() => {
+                i18n.changeLanguage("kz");
+              }}
+            >
+              қаз |
+            </Link>
+            <Link
+              to="#"
+              className="navbar-link"
+              style={{ textDecoration: "none", padding: "2px" }}
+              onClick={() => {
+                i18n.changeLanguage("en");
+              }}
+            >
+              eng
+            </Link>
+          </Link>
           <Link
             to="/about"
             className="navbar-link"
-            style={{ textDecoration: 'none' }}
-            onClick={this.handleExpand}>
+            style={{ textDecoration: "none" }}
+            onClick={this.handleExpand}
+          >
             О конкурсе
           </Link>
           <Link
             to="/organizers"
             className="navbar-link"
-            style={{ textDecoration: 'none' }}
-            onClick={this.handleExpand}>
+            style={{ textDecoration: "none" }}
+            onClick={this.handleExpand}
+          >
             Об организаторах конкурса
           </Link>
           <Link
             to="/archive"
             className="navbar-link"
-            style={{ textDecoration: 'none' }}
-            onClick={this.handleExpand}>
+            style={{ textDecoration: "none" }}
+            onClick={this.handleExpand}
+          >
             Архив
           </Link>
           <Link
             to="/participation"
             className="navbar-link"
-            style={{ textDecoration: 'none' }}
-            onClick={this.handleExpand}>
+            style={{ textDecoration: "none" }}
+            onClick={this.handleExpand}
+          >
             Участие
           </Link>
           <Link
             to="/results"
             className="navbar-link"
-            style={{ textDecoration: 'none' }}
-            onClick={this.handleExpand}>
+            style={{ textDecoration: "none" }}
+            onClick={this.handleExpand}
+          >
             Итоги
           </Link>
         </div>
